3c9a4b388fa5dcb516476a4327dcd5152da4801f
katchowski <jt.jkaowski@gmail.com>
2023-12-14 13:38:50 -0600

    autocorrect v1.2

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.io.*; //imports all io packages
 public class Main { //main class
     static ArrayList<String> symbols = new ArrayList<>(); //list of symbols
     static ArrayList<Integer> nums = new ArrayList<>(); //list of numbers
+    static boolean print = true; //boolean for formatting
     public static void main(String[] args) { //main method
         ArrayList<String> words = new ArrayList<>(); //big wordlist
         nums = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)); //adds numbers to the list
@@ -18,50 +19,64 @@ public class Main { //main class
         } catch(FileNotFoundException error) { //catches FileNotFoundException errors
             System.out.println("file not found"); // catches FileNotFoundException errors
         }
-
-        String input = in(); //gets user input
-        if (input.split(" ").length > 1) { //checks if the input are multiple words
-            ArrayList<String> userin = parse(input); //parses the input
+        if (args.length > 0) {
+            ArrayList<String> userin = new ArrayList<>(Arrays.asList(args));
+            print = userin.get(0).equalsIgnoreCase("true");
             for (String item : userin) { //loops through every word in the input
                 correct(words, item); //checks if the word is correct
             }
         } else {
-            correct(words, input); //checks if the word is correct
+            String input = in(); //gets user input
+            if (input.split(" ").length > 1) { //checks if the input are multiple words
+                ArrayList<String> userin = parse(input); //parses the input
+                for (String item : userin) { //loops through every word in the input
+                    correct(words, item); //checks if the word is correct
+                }
+            } else {
+                correct(words, input); //checks if the word is correct
+            }
         }
     }
 
-    public static void correct(ArrayList<String> words, String item) {
-        if (!(check(words, item))) { //checks if the word is in the dictionary
-            boolean start = true; //boolean for formatting
-            boolean end = false; //boolean for formatting
-            int i = 0; //index for formatting
-            ArrayList<String> similar = getsimilar(item, words); //gets similar words
-            if (similar.size() > 0) {
-                System.out.print(item + ", is incorrect, did you mean: "); //prints similar words
-                if (similar.size() == 1) { //checks if there is only one similar word
-                    System.out.println(similar.get(0) + "?"); //prints the similar word
-                } else {
-                    for (String w : similar) { //loops through similar words
-                        if (i == similar.size() - 1) {
-                            end = true; //sets end to true if the index is the last
+    public static ArrayList<String> correct(ArrayList<String> words, String item) {
+        if (print) {
+            if (!(check(words, item))) { //checks if the word is in the dictionary
+                boolean start = true; //boolean for formatting
+                boolean end = false; //boolean for formatting
+                int i = 0; //index for formatting
+                ArrayList<String> similar = getsimilar(item, words); //gets similar words
+                if (similar.size() > 0) {
+                    System.out.print(item + ", is incorrect, did you mean: "); //prints similar words
+                    if (similar.size() == 1) { //checks if there is only one similar word
+                        System.out.println(similar.get(0) + "?"); //prints the similar word
+                    } else {
+                        for (String w : similar) { //loops through similar words
+                            if (i == similar.size() - 1) {
+                                end = true; //sets end to true if the index is the last
+                            }
+                            if (start) {
+                                System.out.print(w); //prints the first word
+                                start = false; //sets start to false
+                            } else if (end) {
+                                System.out.println(", " + w + "?"); //prints the last word
+                            } else {
+                                System.out.print(", " + w); //prints the middle words
+                            }
+                            i++; //increments index
                         }
-                        if (start) {
-                            System.out.print(w); //prints the first word
-                            start = false; //sets start to false
-                        } else if (end) {
-                            System.out.println(", " + w + "?"); //prints the last word
-                        } else {
-                            System.out.print(", " + w); //prints the middle words
-                        }
-                        i++; //increments index
                     }
+                } else {
+                    System.out.print(item + ", is incorrect, no similar words found."); //prints this if there are no similar words
                 }
-            } else {
-                System.out.print(item + ", is incorrect, no similar words found."); //prints this if there are no similar words
+                if (similar.size() == 0) {
+                    System.out.println();
+                } //prints a new line if there are no similar words
+            } else { //if the word is in the dictionary
+                System.out.println(item + ", is correct."); //prints this if the word is in the dictionary <-(I thought I spelled this wrong but the program tells me otherwise)
             }
-            if (similar.size() == 0) {System.out.println();} //prints a new line if there are no similar words
-        } else { //if the word is in the dictionary
-            System.out.println(item + ", is correct."); //prints this if the word is in the dictionary <-(I thought I spelled this wrong but the program tells me otherwise)
+            return null;
+        } else {
+            return getsimilar(item, words);
         }
     }
 
